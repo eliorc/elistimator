38fe80f75a472dffbb8a8036db79ad9af6355580
Elior Cohen <elior.cohen.p@gmail.com>
2019-02-01 12:53:49 +0200

    Fully functional from_disk, continues training, evaluates and predicts properly

--- src/elistimator/elistimator.py
+++ src/elistimator/elistimator.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 from itertools import count
 from contextlib import suppress
+import pickle
+import abc
 
 from tqdm import tqdm
 import numpy as np
@@ -18,8 +20,19 @@ InputFn = Callable[[], tf.data.Dataset]
 
 # </editor-fold>
 
+class ElistimatorSpec(abc.ABC):
 
-class TrainSpec:
+    @abc.abstractmethod
+    def to_dict(self) -> dict:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def from_dict(graph: tf.Graph, dictionary: dict) -> 'ElistimatorSpec':
+        pass
+
+
+class TrainSpec(ElistimatorSpec):
 
     def __init__(self,
                  loss: tf.Tensor,
@@ -39,8 +52,19 @@ class TrainSpec:
         if create_viz_op:
             tf.summary.scalar('loss', self.loss)
 
+    def to_dict(self) -> dict:
+        return {
+            'loss': self.loss.name,
+            'train_op': self.train_op.name}
 
-class EvaluationSpec:
+    @staticmethod
+    def from_dict(graph: tf.Graph, dictionary: dict) -> 'TrainSpec':
+        return TrainSpec(loss=graph.get_tensor_by_name(dictionary['loss']),
+                         train_op=graph.get_operation_by_name(dictionary['train_op']),
+                         create_viz_op=False)
+
+
+class EvaluationSpec(ElistimatorSpec):
 
     def __init__(self,
                  loss: tf.Tensor,
@@ -63,6 +87,30 @@ class EvaluationSpec:
 
         return running_vars
 
+    def to_dict(self):
+
+        # Metric ops
+        dictionary = dict()
+        dictionary['metric_ops'] = {key: (tensor.name, op.name) for key, (tensor, op) in self.metric_ops.items()}
+
+        # Loss
+        dictionary['loss'] = self.loss.name
+
+        return dictionary
+
+    @staticmethod
+    def from_dict(graph: tf.Graph, dictionary: dict) -> 'EvaluationSpec':
+
+        # Metric ops
+        eval_metric_ops = {key: (graph.get_tensor_by_name(tensor), graph.get_tensor_by_name(op)) for
+                           key, (tensor, op) in dictionary['metric_ops'].items()}
+
+        # Loss
+        loss = graph.get_tensor_by_name(dictionary['loss'])
+
+        return EvaluationSpec(loss=loss,
+                              eval_metric_ops=eval_metric_ops)
+
     @staticmethod
     def _get_scope(tensor_name: str) -> Optional[str]:
         """
@@ -79,7 +127,7 @@ class EvaluationSpec:
         return split_name[-2]
 
 
-class PredictSpec:
+class PredictSpec(ElistimatorSpec):
 
     def __init__(self,
                  output: Dict[str, tf.Tensor]):
@@ -90,9 +138,17 @@ class PredictSpec:
 
         self.output = output
 
+    def to_dict(self):
+        return {key: tensor.name for key, tensor in self.output.items()}
 
-class Estimator:
+    @staticmethod
+    def from_dict(graph: tf.Graph, dictionary: dict) -> 'PredictSpec':
+        return PredictSpec(output={key: graph.get_tensor_by_name(tensor) for key, tensor in dictionary.items()})
+
+
+class Elistimator:
     TQDM_NCOLS = 80
+    ELISTIMATOR_META_FILENAME = 'elistimator_meta'
 
     @property
     def model_dir(self) -> Path:
@@ -129,14 +185,19 @@ class Estimator:
     def __init__(self,
                  model_fn: ModelFn,
                  model_dir: Optional[str] = None,
-                 params: Optional[dict] = None):
+                 params: Optional[dict] = None,
+                 session_args: Optional[dict] = None):
         """
         Creates a new instance of the Estimator class
 
         :param model_fn: Model creating function
         :param model_dir: Directory to host all files related to the model
         :param params: Parameters to be available to the model_fn function
+        :param session_args: Parameters to pass to the tf.Session
         """
+
+        if not session_args:
+            session_args = {}
 
         self._model_fn = model_fn
         self._model_dir = self._get_or_create_directory(model_dir)
@@ -145,7 +206,6 @@ class Estimator:
         self._train_size = None
         self._validation_size = None
         self._train_count = 0
-        self._validation_count = 0
 
         # To be defined with the setup_training method
         self._handle = None
@@ -162,9 +222,8 @@ class Estimator:
         self._input_signature = None
         self._train_summary_writer = None
         self._validation_summary_writer = None
-        self._train_saver = None
-        self._session = None
-        self._graph = None
+        self._graph = tf.Graph()
+        self._session = tf.Session(graph=self._graph, **session_args)
         self._last_validation_metrics = None
         self._checkpoints = dict()
 
@@ -172,7 +231,7 @@ class Estimator:
               train_input_fn: InputFn,
               validation_input_fn: Optional[InputFn] = None,
               data_keys: Optional[List[str]] = None,
-              train_saver_options: Optional[dict] = None) -> 'Estimator':
+              train_saver_options: Optional[dict] = None) -> 'Elistimator':
         """
         Setup training flow, should be called only once.
 
@@ -191,11 +250,9 @@ class Estimator:
         # If data_keys not provided, use 0, 1, 2 ...
         data_keys = data_keys if data_keys else count()
 
-        self._graph = tf.Graph()
-
         with self._graph.as_default():
 
-            tf.train.create_global_step()
+            tf.train.get_or_create_global_step()
 
             with tf.variable_scope('input'):
 
@@ -203,7 +260,8 @@ class Estimator:
                 train_dataset = train_input_fn()
                 train_iterator = train_dataset.make_initializable_iterator()
 
-                self._is_training = tf.placeholder_with_default(False, shape=[], name='is_training')
+                if self._is_training is None:
+                    self._is_training = tf.placeholder_with_default(False, shape=[], name='is_training')
 
                 if validation_input_fn:
                     # Setup validation data pipeline
@@ -211,62 +269,72 @@ class Estimator:
                     validation_iterator = validation_dataset.make_initializable_iterator()
 
                 # Iterators handling
-                output_types = train_dataset.output_types
-                output_shapes = train_dataset.output_shapes
-                handle = tf.placeholder(tf.string, shape=[], name='iterator_handle')
-                iterator = tf.data.Iterator.from_string_handle(handle,
-                                                               output_types=output_types,
-                                                               output_shapes=output_shapes)
+                data = {}
+                if self._handle is None:
+                    output_types = train_dataset.output_types
+                    output_shapes = train_dataset.output_shapes
+                    handle = tf.placeholder(tf.string, shape=[], name='iterator_handle')
+                    iterator = tf.data.Iterator.from_string_handle(handle,
+                                                                   output_types=output_types,
+                                                                   output_shapes=output_shapes)
 
-                iterator_output = iterator.get_next()
+                    iterator_output = iterator.get_next()
 
-                # Determine whether to assign keys or already supplied by dataset
-                if isinstance(iterator_output, dict):
-                    data = iterator_output
-                else:
-                    data = {data_key: tf.identity(data_tensor, name=data_key)
-                            for data_key, data_tensor in zip(data_keys, iterator_output)}
+                    # Determine whether to assign keys or already supplied by dataset
+                    if isinstance(iterator_output, dict):
+                        data = iterator_output
+                    else:
+                        data = {data_key: tf.identity(data_tensor, name=data_key)
+                                for data_key, data_tensor in zip(data_keys, iterator_output)}
 
             train_spec, validation_spec, predict_spec = self._model_fn(data, self._is_training, self._params)
 
             # Prepare running variables initializer for metric's running variables
-            if validation_spec:
+            if validation_spec and self._running_variables_initializer is None:
                 self._running_variables_initializer = tf.variables_initializer(
                     var_list=validation_spec.get_running_variables())
 
             # Create visualization op
-            self._visualization_op = tf.summary.merge_all()
+            if self._visualization_op is None:
+                self._visualization_op = tf.summary.merge_all()
 
             # Create a train saver
             self._train_saver = tf.train.Saver() if not train_saver_options else tf.train.Saver(**train_saver_options)
 
         # Create summary logs directories
-        if validation_input_fn:
+        if validation_input_fn and not self.train_logs.exists():
             self.train_logs.mkdir(parents=True)
-        if validation_input_fn:
+        if validation_input_fn and not self.validation_logs.exists():
             self.validation_logs.mkdir(parents=True)
 
-        # Create checkpoints diretory
-        self.train_checkpoints.mkdir(parents=True)
+        # Create checkpoints directory
+        if not self.train_checkpoints.exists():
+            self.train_checkpoints.mkdir(parents=True)
 
         # Assign
-        self._session = tf.Session(graph=self._graph)
         self._train_handle = self._session.run(train_iterator.string_handle('train_handle')) if train_iterator else None
         self._train_iterator = train_iterator
         self._validation_handle = self._session.run(validation_iterator.string_handle(
             'validation_handle')) if validation_iterator else None
         self._validation_iterator = validation_iterator
-        self._handle = handle
+
+        if self._handle is None:
+            self._handle = handle
+
         self._train_spec = train_spec
         self._evaluation_spec = validation_spec
         self._predict_spec = predict_spec
-        self._input_signature = {data_key: data_tensor.name for data_key, data_tensor in data.items()}
+
+        if not self._input_signature:
+            self._input_signature = {data_key: data_tensor.name for data_key, data_tensor in data.items()}
 
         self._train_summary_writer = tf.summary.FileWriter(logdir=str(self.train_logs),
                                                            graph=self._graph)
         if validation_input_fn:
             self._validation_summary_writer = tf.summary.FileWriter(logdir=str(self.validation_logs),
                                                                     graph=self._graph)
+        # First time initialization
+        self._session.run(self._train_iterator.initializer)
 
         return self
 
@@ -278,7 +346,7 @@ class Estimator:
             self._session.run(tf.global_variables_initializer())
 
     def train(self,
-              max_steps: Optional[int] = None) -> 'Estimator':
+              max_steps: Optional[int] = None) -> 'Elistimator':
         """
         Train. Runs for given number of steps or until iterator is exhausted
 
@@ -303,10 +371,6 @@ class Estimator:
         train_pbar = tqdm(total=self._train_size,
                           desc='Training ({})'.format(self._train_count),
                           ncols=self.TQDM_NCOLS)
-
-        # First time initialization
-        if not self._session.run(global_step):
-            self._session.run(self._train_iterator.initializer)
 
         # Training
         while local_step < max_steps:
@@ -561,6 +625,9 @@ class Estimator:
         for key_to_remove in keys_to_remove:
             del self._checkpoints[key_to_remove]
 
+        # Save Elistimator meta data
+        self._save_meta()
+
         return ckpt_file
 
     def restore_ckpt(self, path_prefix: str):
@@ -580,7 +647,7 @@ class Estimator:
         """
 
         # Get best or latest checkpoint
-        best_checkpoint = sorted(self._checkpoints.items(), key=lambda k_v: k_v[1]['metrics'].get('loss', -k_v[0]))[0][1]['file']
+        best_checkpoint = self._best_checkpoint()['file']
 
         self.restore_ckpt(path_prefix=best_checkpoint)
 
@@ -589,7 +656,28 @@ class Estimator:
         Restores graph and variables from the latest checkpoint saved
         """
 
-        self.restore_ckpt(path_prefix=self._train_saver.last_checkpoints[-1])
+        # Get latest checkpoint
+        latest_checkpoint = self._latest_checkpoint()['file']
+
+        self.restore_ckpt(path_prefix=latest_checkpoint)
+
+    def _best_checkpoint(self) -> dict:
+        """
+        Gets best checkpoint entry from the
+
+        :return: Best checkpoint entry
+        """
+
+        return sorted(self._checkpoints.items(), key=lambda k_v: k_v[1]['metrics'].get('loss', -k_v[0]))[0][1]
+
+    def _latest_checkpoint(self) -> dict:
+        """
+        Gets latest checkpoint entry from the
+
+        :return: Latest checkpoint entry
+        """
+
+        return sorted(self._checkpoints.items(), key=lambda k_v: -k_v[0])[0][1]
 
     def _predict_from_dataset(self, input_fn: InputFn,
                               is_training: Optional[bool] = False) -> Generator[dict, None, None]:
@@ -646,3 +734,114 @@ class Estimator:
             model_dir = Path(model_dir.name)
 
         return model_dir
+
+    def _save_meta(self):
+        """
+        Saves Estimators attributes
+
+        """
+
+        attrs_to_save = {
+            '_handle': self._handle.name,
+            '_train_spec': self._train_spec.to_dict() if self._train_spec else self._train_spec,
+            '_evaluation_spec': self._evaluation_spec.to_dict() if self._evaluation_spec else self._evaluation_spec,
+            '_predict_spec': self._predict_spec.to_dict() if self._predict_spec else self._predict_spec,
+            '_is_training': self._is_training.name,
+            '_running_variables_initializer': self._running_variables_initializer.name,
+            '_visualization_op': self._visualization_op.name,
+            '_input_signature': self._input_signature,
+            '_checkpoints': self._checkpoints,
+            '_train_count': self._train_count}
+
+        with open(str(self.train_checkpoints / self.ELISTIMATOR_META_FILENAME), 'wb') as e_meta:
+            pickle.dump(obj=attrs_to_save, file=e_meta)
+
+    @staticmethod
+    def _load_meta(meta_file: str) -> dict:
+        """
+        Loads an Elistimator meta file from disk
+
+        :param meta_file: Meta file path
+        :return: Elistimator meta data
+        """
+
+        with open(meta_file, 'rb') as mf:
+            return pickle.load(mf)
+
+    @staticmethod
+    def from_disk(model_dir: str,
+                  from_best: Optional[bool] = False,
+                  session_args: Optional[dict] = None) -> 'Elistimator':
+        """
+        Load an Estimator from disk. Requires at least one checkpoint save.
+
+        :param model_dir: Model directory
+        :param from_best: Restore best checkpoint, if False, restores latest.
+        :param session_args: Parameters to pass to the tf.Session
+        :return: Estimator
+        """
+
+        model_dir = Path(model_dir)
+
+        # Check whether Estimator was saved at least once
+        meta_file = model_dir / 'checkpoints' / Elistimator.ELISTIMATOR_META_FILENAME
+        if not meta_file.exists():
+            raise ValueError('No checkpoints to restore from')
+
+        # Get Estimator meta data
+        meta = Elistimator._load_meta(str(meta_file))
+
+        # Init empty Estimator
+        estimator = Elistimator(model_fn=None,
+                                model_dir=str(model_dir),
+                                session_args=session_args)
+        # Restore input signature
+        estimator._input_signature = meta['_input_signature']
+
+        # Restore checkpoints
+        estimator._checkpoints = meta['_checkpoints']
+
+        # Restore saver
+        meta_graph = estimator._best_checkpoint() if from_best else estimator._latest_checkpoint()
+        with estimator._graph.as_default():
+            estimator._train_saver = tf.train.import_meta_graph(meta_graph_or_file='{}.meta'.format(meta_graph['file']))
+
+            # Restore running vars initializer
+            estimator._running_variables_initializer = estimator._graph.get_operation_by_name(
+                meta['_running_variables_initializer'])
+
+        # Restore graph from checkpoint
+        if from_best:
+            estimator.restore_best_ckpt()
+        else:
+            estimator.restore_latest_ckpt()
+
+        # Restore Elistimator specs
+        train_spec = TrainSpec.from_dict(graph=estimator.graph, dictionary=meta['_train_spec']) \
+            if meta['_train_spec'] else None
+        evaluation_spec = EvaluationSpec.from_dict(graph=estimator.graph, dictionary=meta['_evaluation_spec']) \
+            if meta['_evaluation_spec'] else None
+        predict_spec = PredictSpec.from_dict(graph=estimator.graph, dictionary=meta['_predict_spec']) \
+            if meta['_predict_spec'] else None
+
+        estimator._train_spec = train_spec
+        estimator._evaluation_spec = evaluation_spec
+        estimator._predict_spec = predict_spec
+
+        # Restore model_fn
+        model_fn = lambda *args: (train_spec, evaluation_spec, predict_spec)
+        estimator._model_fn = model_fn
+
+        # Restore handle
+        estimator._handle = estimator._graph.get_tensor_by_name(meta['_handle'])
+
+        # Restore is_training placeholder
+        estimator._is_training = estimator._graph.get_tensor_by_name(meta['_is_training'])
+
+        # Restore visualization op
+        estimator._visualization_op = estimator._graph.get_tensor_by_name(meta['_visualization_op'])
+
+        # Restore train count
+        estimator._train_count = meta['_train_count']
+
+        return estimator
